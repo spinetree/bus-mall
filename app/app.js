ab1cc54cc5570cf5b8837c6c1295f68fb2e1ce13
spinetree <steve@spinetree.org>
2019-08-19 13:19:09 -0700

    wrote pseudocode

---
+++ app/app.js
@@ -0,0 +1,29 @@
+// a list of images that we've got in a folder
+// a list of the images we just saw
+// a variable for the number of times we've voted (at 25, it stops and ...???)
+
+// a constructor function that turns each image from the list into an object with title etc.
+// with a number for total appearances
+// with a number for times it got choesn
+
+// a prototype that does
+// let each image alter its stats when a vote happens
+// and add itself to the list of images we just saw ()
+
+// load images from the list into constructor
+
+// choose a random image
+// add to a list of randomly chosen candidate images
+// check that random image against the list of just-shown ones
+// if it's on the list, get a new candidate
+// if it's already on the candidate list, also get a new candidate
+// add it to a list of the candidate images
+
+// loop through the candidate images
+// create a figure for each
+
+// when I click on a figure
+// ++ that ones's total votes
+// ++ everyone's total appearances
+// wipe out the list of last round
+// add everyone to the list of last round
