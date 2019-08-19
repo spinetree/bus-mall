9251c9a0548a9529a04677893359efcaa4b0dbc8
spinetree <steve@spinetree.org>
2019-08-19 14:45:57 -0700

    wrote userstories and pfffft shouldla done that first because wold've written better pseudocode the first time if I had

--- app/app.js
+++ app/app.js
@@ -1,29 +1,71 @@
 // a list of images that we've got in a folder
 // a list of the images we just saw
-// a variable for the number of times we've voted (at 25, it stops and ...???)
+// a variable for the number of times we've voted
 
-// a constructor function that turns each image from the list into an object with title etc.
-// with a number for total appearances
-// with a number for times it got choesn
-
-// a prototype that does
-// let each image alter its stats when a vote happens
-// and add itself to the list of images we just saw ()
 
 // load images from the list into constructor
 
+
+// a constructor function
+// -----------------------
+// that turns each image from the list into an object with
+// title
+// ID (we need a unique id to use when checking an image against the already-done list)
+// with a number for total appearances
+// with a number for times it got choesn
+// and a hit %
+
+
+// prototype
+// -----------------------
+// let each product alter its stats when a vote happens
+// product.won() = ++ my hit total
+// recalc my hit %
+// product.lost() = ++ my appearances total
+// recalc my hit %
+// and add itself(its index? yes) to the list of images we just saw ()
+
+
+// render the test mechanism
+// -----------------------
+
+// while candidate images < 3
 // choose a random image
-// add to a list of randomly chosen candidate images
 // check that random image against the list of just-shown ones
-// if it's on the list, get a new candidate
-// if it's already on the candidate list, also get a new candidate
-// add it to a list of the candidate images
+// if it's on the list, run this function again
+// check if it's on the candidate list already
+// if it's on the list, run this function again
+// else
+// add to the list of candidate images
 
 // loop through the candidate images
 // create a figure for each
+// append those to the dom
 
+
+// event listener - user clicks a thing
+// -----------------------
 // when I click on a figure
 // ++ that ones's total votes
 // ++ everyone's total appearances
+// increase my vote number ++
+// if it's 25
+// render the results list
+// else
 // wipe out the list of last round
 // add everyone to the list of last round
+// re-render the test mechanism
+
+
+// render a results list
+// -----------------------
+// crawl each object and get its url, name, total clicks and total votes
+// get #results-list
+
+// for each object create a li containing name etc. and a decimal of its hit %
+// push them shits into an array
+// use .sort on that array
+
+// fill #results-list with the sorted array
+
+
